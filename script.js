7f7991a6cae53c2f61dc504a3281fee300e50d85
Dhirajraje <dhirajraje112@gmail.com>
2020-07-05 16:58:25 +0530

    Mobile view -> avatar,name,bio placed.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var profileData = {
     "Name"  : "Dhiraj Salunke",
     "Email" : "dhirajraje113@gmail.com",
+    "Bio"   : "I am a passionate django-web flutter-firebase developer and a linux geek. Looking forward to work with organization where I can use my knoledge and skills for organizations groth and my own developement.",
     "phone" : "+91 9923454467",
     "GitHub": "https://github.com/Dhirajraje",
     "LinkedIn"  : "https://www.linkedin.com/in/dhiraj-salunke-702564183/",
@@ -9,6 +10,9 @@ var profileData = {
 }
 
 
+yourName = document.getElementById('your-name');
+bio = document.getElementById('bio');
 
 
-console.log(profileData["Email"])
+yourName.innerHTML = profileData['Name'];
+bio.innerHTML = profileData['Bio'];
