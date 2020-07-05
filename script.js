59226f89d39a1f45d03dca5c5c2948913b7c8b88
Dhirajraje <dhirajraje112@gmail.com>
2020-07-05 14:09:51 +0530

    Work in progress but styled some aspects.

---
+++ script.js
@@ -0,0 +1,14 @@
+// your own data here
+var profileData = {
+    "Name"  : "Dhiraj Salunke",
+    "Email" : "dhirajraje113@gmail.com",
+    "phone" : "+91 9923454467",
+    "GitHub": "https://github.com/Dhirajraje",
+    "LinkedIn"  : "https://www.linkedin.com/in/dhiraj-salunke-702564183/",
+    "Instagram" : "https://instagram.com/_dhirajraje_"
+}
+
+
+
+
+console.log(profileData["Email"])
