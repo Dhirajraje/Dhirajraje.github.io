88304dce3c80ec9f41272e7e544916d542e1c56c
Dhirajraje <dhirajraje113@gmail.com>
2020-09-23 10:52:30 +0530

    resume updated.

--- script.js
+++ script.js
@@ -2,6 +2,34 @@ var detailWindow = document.getElementById('main-card');
 var cards = document.getElementsByClassName('detail-card');
 var cardIds = ['showcase', 'resume', 'skills', 'contact'];
 var closeButton = document.getElementsByClassName('close');
+var content = document.body.innerHTML;
+
+function isBetween(min,max,val){
+    if(val>=min&&val<=max)
+        return true;
+    else
+        return false;
+}
+function resizeMsg(){
+    var errorMsg = "<div id='Error-Msg'>We currently support aspect ratios between 1.73 to 2.35, So kindly resize your window or Rotate your phone.</div>";
+    if(!isBetween(1.73,2.35,window.innerWidth/window.innerHeight)){
+        document.body.removeChild(document.body.firstChild);
+        document.body.innerHTML = errorMsg;
+    }
+    else
+        if(document.body.innerHTML == errorMsg)
+            window.close;
+    console.log(document.body);
+}
+
+// window size issue
+
+resizeMsg();
+
+window.addEventListener('resize',()=>{
+    resizeMsg();
+})
+
 // Close Window
 
 function closeWindowCompletely(card) {
@@ -52,8 +80,41 @@ document.getElementById('contact').addEventListener('click', () => {
     openWindow(cards[3]);
 })
 
+// Download resume
 
 document.getElementsByClassName('button')[4].addEventListener('click', () => {
     event.preventDefault();
     window.open('Assets/Resume.pdf', '_blank');
+})
+
+// Contanct details
+
+document.getElementById('Email').addEventListener('click', () => {
+    event.preventDefault();
+    window.open('mailto:dhirajraje113@gmail.com', '_blank');
+})
+
+document.getElementById('Mobile').addEventListener('click', () => {
+    event.preventDefault();
+    window.open('tel:+91 9923454467', '_blank');
+})
+
+document.getElementById('LinkedIn').addEventListener('click', () => {
+    event.preventDefault();
+    window.open('https://google.com', '_blank');
+})
+
+document.getElementById('Github').addEventListener('click', () => {
+    event.preventDefault();
+    window.open('https://github.com/Dhirajraje', '_blank');
+})
+
+document.getElementById('Facebook').addEventListener('click', () => {
+    event.preventDefault();
+    window.open('http://google.com', '_blank');
+})
+
+document.getElementById('Instagram').addEventListener('click', () => {
+    event.preventDefault();
+    window.open('http://google.com', '_blank');
 })
