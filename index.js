d3eed5e0ee906f2e0e61dcaa7090dcd729fbf764
Dhiraj Salunke <dhiraj.salunke@flentas.com>
2021-11-05 10:48:01 +0530

    sidenav theme.

---
+++ index.js
@@ -0,0 +1,13 @@
+function closeDrawer(){
+    var drawer = document.querySelector('.drawer');
+    drawer.style.left = '-100%';
+    var arrow = document.querySelector('.arrow');
+    arrow.style.display = 'block';
+}
+
+function openDrawer(){
+    var drawer = document.querySelector('.drawer');
+    drawer.style.left = '0%';
+    var arrow = document.querySelector('.arrow');
+    arrow.style.display = 'none';
+}
