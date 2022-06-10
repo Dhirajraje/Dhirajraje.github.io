ae7513035ae9fee7b3ba6d138e7f1d34abe5711b
Dhiraj Salunke <dhirajraje113@gmail.com>
2022-06-10 21:11:39 +0530

    svelte.

---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+import Landing from "$lib/components/landing.svelte";
+import Skills from "$lib/components/skills.svelte";
+import Experience from "$lib/components/experience.svelte";
+import Footer from "$lib/components/footer.svelte";
+
+</script>
+
+<main>
+    <Landing />
+    <Skills />
+    <span class="h-48 p-48 text-white">-</span>
+    <Experience />
+    <Footer />
+</main>
