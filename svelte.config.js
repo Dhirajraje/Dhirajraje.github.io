b20d87eccaace39621cb0ce37382563d43d87c07
Dhiraj Salunke <dhirajraje113@gmail.com>
2022-06-11 11:44:56 +0530

    vercel adapter.

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from "svelte-preprocess";
 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false
-		}),
-		prerender:{
-			default:true
-		}
+		adapter: vercel()
 	},
 	preprocess: [
 		preprocess({
