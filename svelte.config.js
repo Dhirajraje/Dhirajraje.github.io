472b24a96dc0b9f0767bcb0cee59e24971d6e07e
Dhiraj Salunke <dhirajraje113@gmail.com>
2022-06-15 01:26:04 +0530

    Static router

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,23 @@
 import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from "svelte-preprocess";
 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: vercel()
+		adapter: adapter({
+			// default options are shown
+			pages: 'docs',
+			assets: 'docs',
+			fallback: null,
+			precompress: false
+		}),
+
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
 	},
 	preprocess: [
 		preprocess({
