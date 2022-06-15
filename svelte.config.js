7e1f4b9ae24579175cc9ced82840b3a6e0ab2320
Dhiraj Salunke <dhirajraje113@gmail.com>
2022-06-15 23:27:36 +0530

    vercel adapter.

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,11 @@
 import vercel from '@sveltejs/adapter-vercel';
-import adapter from '@sveltejs/adapter-static';
 import preprocess from "svelte-preprocess";
 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: 'docs',
-			assets: 'docs',
-			fallback: null,
-			precompress: false
-		}),
-
-		prerender: {
-			// This can be false if you're using a fallback (i.e. SPA mode)
-			default: true
-		}
+		adapter: vercel()
 	},
 	preprocess: [
 		preprocess({
